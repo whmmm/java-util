6b47f0f18da05d6749560ba231784c101d3c2d28
whmmm <942582838@qq.com>
2023-06-14 15:21:53 +0800

    添加测试

---
+++ src/test/java/org/whmmm/util/DataTest.java
@@ -0,0 +1,236 @@
+package org.whmmm.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * <p><b> ----------------------- </b></p>
+ * <p><b> author: whmmm           </b></p>
+ * <p><b> date  : 2023/4/27 15:09 </b></p>
+ *
+ * @author whmmm
+ */
+public class DataTest {
+    @Test
+    public void test() {
+        String str = "[\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"冯唐欣\",\n" +
+                     "                \"taskId\": 1396048260200300544,\n" +
+                     "                \"releaseId\": 1396048260116414464,\n" +
+                     "                \"stuId\": 1355599658531586048,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（2）班\",\n" +
+                     "                \"classId\": 1190226295445258240,\n" +
+                     "                \"stuNumber\": \"29070025\",\n" +
+                     "                \"fmtStuNumber\": \"29070025\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:26\",\n" +
+                     "                \"totalScore\": 46.00,\n" +
+                     "                \"subjectiveScore\": 16.00,\n" +
+                     "                \"objectiveScore\": 30.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355599658531586048,\n" +
+                     "                    \"testNumber\": \"29070025\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"付浩博\",\n" +
+                     "                \"taskId\": 1396048263916453889,\n" +
+                     "                \"releaseId\": 1396048263815790592,\n" +
+                     "                \"stuId\": 1355610235047936000,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（6）班\",\n" +
+                     "                \"classId\": 1249273103747907584,\n" +
+                     "                \"stuNumber\": \"29070027\",\n" +
+                     "                \"fmtStuNumber\": \"29070027\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:26\",\n" +
+                     "                \"totalScore\": 52.00,\n" +
+                     "                \"subjectiveScore\": 14.00,\n" +
+                     "                \"objectiveScore\": 38.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355610235047936000,\n" +
+                     "                    \"testNumber\": \"29070027\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"韩书俊\",\n" +
+                     "                \"taskId\": 1396048262163234816,\n" +
+                     "                \"releaseId\": 1396048262054182912,\n" +
+                     "                \"stuId\": 1355601861514264576,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（4）班\",\n" +
+                     "                \"classId\": 1190226303909363712,\n" +
+                     "                \"stuNumber\": \"29070037\",\n" +
+                     "                \"fmtStuNumber\": \"29070037\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:29\",\n" +
+                     "                \"totalScore\": 59.00,\n" +
+                     "                \"subjectiveScore\": 21.00,\n" +
+                     "                \"objectiveScore\": 38.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355601861514264576,\n" +
+                     "                    \"testNumber\": \"29070037\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"何锦弘\",\n" +
+                     "                \"taskId\": 1396048263060815873,\n" +
+                     "                \"releaseId\": 1396048262934986752,\n" +
+                     "                \"stuId\": 1355610198674931712,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（5）班\",\n" +
+                     "                \"classId\": 1233743264521650176,\n" +
+                     "                \"stuNumber\": \"29070039\",\n" +
+                     "                \"fmtStuNumber\": \"29070039\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:29\",\n" +
+                     "                \"totalScore\": 44.00,\n" +
+                     "                \"subjectiveScore\": 14.00,\n" +
+                     "                \"objectiveScore\": 30.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355610198674931712,\n" +
+                     "                    \"testNumber\": \"29070039\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"牟俪霏\",\n" +
+                     "                \"taskId\": 1396048261106270209,\n" +
+                     "                \"releaseId\": 1396048261047549952,\n" +
+                     "                \"stuId\": 1280728125786292224,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（3）班\",\n" +
+                     "                \"classId\": 1004732694329819136,\n" +
+                     "                \"stuNumber\": \"29070096\",\n" +
+                     "                \"fmtStuNumber\": \"29070096\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:47\",\n" +
+                     "                \"totalScore\": 69.00,\n" +
+                     "                \"subjectiveScore\": 31.00,\n" +
+                     "                \"objectiveScore\": 38.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1280728125786292224,\n" +
+                     "                    \"testNumber\": \"29070096\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"潘思辰\",\n" +
+                     "                \"taskId\": 1396048259327885314,\n" +
+                     "                \"releaseId\": 1396048259227222016,\n" +
+                     "                \"stuId\": 1355599623551090688,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（1）班\",\n" +
+                     "                \"classId\": 1190226302793678848,\n" +
+                     "                \"stuNumber\": \"29070101\",\n" +
+                     "                \"fmtStuNumber\": \"29070101\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:48\",\n" +
+                     "                \"totalScore\": 66.00,\n" +
+                     "                \"subjectiveScore\": 28.00,\n" +
+                     "                \"objectiveScore\": 38.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355599623551090688,\n" +
+                     "                    \"testNumber\": \"29070101\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"王紫晴\",\n" +
+                     "                \"taskId\": 1396048261181767682,\n" +
+                     "                \"releaseId\": 1396048261047549952,\n" +
+                     "                \"stuId\": 1355589989293522944,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（3）班\",\n" +
+                     "                \"classId\": 1004732694329819136,\n" +
+                     "                \"stuNumber\": \"29070137\",\n" +
+                     "                \"fmtStuNumber\": \"29070137\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:14:59\",\n" +
+                     "                \"totalScore\": 28.00,\n" +
+                     "                \"subjectiveScore\": 4.00,\n" +
+                     "                \"objectiveScore\": 24.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355589989293522944,\n" +
+                     "                    \"testNumber\": \"29070137\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"张静萌\",\n" +
+                     "                \"taskId\": 1396048261131436033,\n" +
+                     "                \"releaseId\": 1396048261047549952,\n" +
+                     "                \"stuId\": 1355589948650717184,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（3）班\",\n" +
+                     "                \"classId\": 1004732694329819136,\n" +
+                     "                \"stuNumber\": \"29070169\",\n" +
+                     "                \"fmtStuNumber\": \"29070169\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:15:09\",\n" +
+                     "                \"totalScore\": 64.00,\n" +
+                     "                \"subjectiveScore\": 20.00,\n" +
+                     "                \"objectiveScore\": 44.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355589948650717184,\n" +
+                     "                    \"testNumber\": \"29070169\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            },\n" +
+                     "            {\n" +
+                     "                \"stuName\": \"张清海\",\n" +
+                     "                \"taskId\": 1396048259336273920,\n" +
+                     "                \"releaseId\": 1396048259227222016,\n" +
+                     "                \"stuId\": 1355599626034118656,\n" +
+                     "                \"statisLeaveOut\": 1,\n" +
+                     "                \"className\": \"七年级（1）班\",\n" +
+                     "                \"classId\": 1190226302793678848,\n" +
+                     "                \"stuNumber\": \"29070171\",\n" +
+                     "                \"fmtStuNumber\": \"29070171\",\n" +
+                     "                \"schoolNumberType\": 2,\n" +
+                     "                \"scanState\": 3,\n" +
+                     "                \"fileCount\": 0,\n" +
+                     "                \"submitTime\": \"2023-04-26 18:15:09\",\n" +
+                     "                \"totalScore\": 57.00,\n" +
+                     "                \"subjectiveScore\": 25.00,\n" +
+                     "                \"objectiveScore\": 32.00,\n" +
+                     "                \"paperState\": -1,\n" +
+                     "                \"allNumber\": {\n" +
+                     "                    \"userId\": 1355599626034118656,\n" +
+                     "                    \"testNumber\": \"29070171\",\n" +
+                     "                    \"fmtStuNumber\": \"\"\n" +
+                     "                }\n" +
+                     "            }\n" +
+                     "        ]";
+
+
+    }
+}
